b969fc3981b6d416aa7f48029917d555981c53e4
Caipo <mr.dino.rex@gmail.com>
2022-06-30 13:44:12 -0700

    cleaned ups

--- Main.py
+++ Main.py
@@ -1,30 +1,20 @@
-#SET UP SHIT
-import os, discord, requests, face_recognition, cv2, os, random, ffmpeg, asyncio
-from dotenv import load_dotenv
-from discord.ext import commands
-from os import listdir
-from os.path import isfile, join
-
-from elosports.elo import Elo
-import time
-import random
-import mysql.connector
-import Swift
+import discord
 import BestGirl
+from os import getenv
 
 
+def main():
+    # load_dotenv()
+    TOKEN = getenv('DISCORD_TOKEN')
+    client = discord.Client()
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
+    @client.event
+    async def on_message(message):
 
-@client.event
-async def on_message(message):
-    global client
-    
-    await BestGirl.poll(message)
-    #await Swift.swift(message, client)
+        await BestGirl.poll(message)
+        # await Swift.swift(message, client)
+    client.run(TOKEN)
 
 
-client.run(TOKEN)
-
+if __name__ == "__main__":
+    main()
