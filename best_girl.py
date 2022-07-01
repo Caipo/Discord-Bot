80ffa0f5611ff29118402af26989b293da06f099
Caipo <mr.dino.rex@gmail.com>
2022-06-30 17:30:00 -0700

    fixed ordered

--- best_girl.py
+++ best_girl.py
@@ -37,8 +37,7 @@ def get_image(name):
         + str(picked)
         + "\";"
     )
-    print( "asdf " , urls[picked][0] )
-    return urls[picked][0]
+    return urls[picked][1]
 
 
 async def poll(message):
